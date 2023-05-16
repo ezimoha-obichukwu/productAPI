973adc6ce5cd018d378270f449421fe507e5d84d
OBI <77929524+ezimoha-obichukwu@users.noreply.github.com>
2023-05-16 11:05:37 +0100

    Update urls.py

--- CODE/urls.py
+++ CODE/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include('api_app.urls')),
-] if settings.DEBUG:
-    urlpatterns += static(
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
